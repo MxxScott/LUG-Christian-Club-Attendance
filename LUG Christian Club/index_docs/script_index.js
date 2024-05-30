6958fe5e6ce30b381a7fdc7439da3f2a32c71519
David-Jacks <djackfaith0.5@gmail.com>
2024-05-30 07:17:45 +0000

    adding authentication

--- LUG Christian Club/index_docs/script_index.js
+++ LUG Christian Club/index_docs/script_index.js
@@ -1,7 +1,11 @@
 // Select the login button, login form, exit logo, and list button elements
 const loginButton = document.getElementById(`login-button`);
 const loginForm = document.getElementById(`login-form`);
-const exitLogo = document.getElementById(`exit-icon`);
+const addmemButton = document.getElementById(`addmem-button`);
+const addmemForm = document.getElementById(`addmem-form`);
+// Get unique IDs for each exit logo
+const exitLoginIcon = document.getElementById(`exit-login-icon`);
+const exitAddMemIcon = document.getElementById(`exit-addmem-icon`);
 const listButton = document.getElementById(`list-button`);
 // selecting login button, the first and second input to check for authentication and also 
 // the addMenber btn on the main page
@@ -27,7 +31,10 @@ function authenUser(firstVal, secondVal)
 loginButton.addEventListener(`click`, () => {
   // Show the login form with a smooth transition
   loginForm.style.display = `grid`; // Show the form
+<<<<<<< HEAD
   // loginForm.style.opacity = 0; // Start with opacity 0
+=======
+>>>>>>> 60c6985207bee957217ede3c2ef18423a35307c4
   loginForm.style.transition = `opacity 0.5s ease-in-out`; // Set transition effect
   setTimeout(() => { // Delay the opacity change
     loginForm.style.opacity = 1; // Set opacity to 1 after the delay
@@ -36,14 +43,35 @@ loginButton.addEventListener(`click`, () => {
   
 });
 
-// Event listener for the exit logo button
-exitLogo.addEventListener(`click`, () => {
+// Event listener for the addmem button
+addmemButton.addEventListener(`click`, () => {
+  // Show the addMem form with a smooth transition
+  addmemForm.style.display = `grid`; // Show the form
+  addmemForm.style.transition = `opacity 0.5s ease-in-out`; // Set transition effect
+  setTimeout(() => { // Delay the opacity change
+    addmemForm.style.opacity = 1; // Set opacity to 1 after the delay
+  }, 10); // 10 milliseconds delay
+});
+
+// Event listener for the exit logo button for login form
+exitLoginIcon.addEventListener(`click`, () => {
   // Hide the login form with a smooth transition
   // loginForm.style.opacity = 1; // Start with opacity 1
   loginForm.style.transition = `opacity 0.5s ease-in-out`; // Set transition effect
   setTimeout(() => { // Delay the opacity change
     loginForm.style.opacity = 0; // Set opacity to 0 after the delay
-    loginForm.style.display = `none`; // Hide the form after the delay
+    loginForm.style.display = `none`; // Hide the form after the delays
+  }, 10); // 10 milliseconds delay
+});
+
+// Event listener for the exit logo button for add member form
+exitAddMemIcon.addEventListener(`click`, () => {
+  // Hide the add member form with a smooth transition
+  addmemForm.style.opacity = 1; // Start with opacity 1
+  addmemForm.style.transition = `opacity 0.5s ease-in-out`; // Set transition effect
+  setTimeout(() => { // Delay the opacity change
+    addmemForm.style.opacity = 0; // Set opacity to 0 after the delay
+    addmemForm.style.display = `none`; // Hide the form after the delays
   }, 10); // 10 milliseconds delay
 });
 
